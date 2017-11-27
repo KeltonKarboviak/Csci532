55145dd3f0c0cb8de8684a3e85dd3dd66359e081
Kelton Karboviak <kelton.karboviak@gmail.com>
2017-11-27 11:09:00 -0600

    Lab4 WIP, modified test_runner.py

--- Lab4/test_runner.py
+++ Lab4/test_runner.py
@@ -1,32 +1,42 @@
 #!/usr/bin/env python
 
-import os
-import sys
+import subprocess
 import itertools
 import time
 import pandas as pd
 
 
-
 def main():
     input_sizes = [32, 64, 128, 256, 512, 1024]
     filter_sizes = [4, 8, 16, 32, 64]
-    
+
     size_combinations = filter(
         lambda (i, f): i >= f,
         itertools.product(input_sizes, filter_sizes)
     )
-    
-    df = pd.DataFrame(index=input_sizes, columns=filter_sizes)    
+
+    df = pd.DataFrame(index=input_sizes, columns=filter_sizes)
+
+    cmds = ['cpu', 'gpu']
+
+    cmd_str = './convolution_{0} {1} {1} {2} {2}'
 
     for i, f in size_combinations:
-        begin = time.time()
-        # Make call here
-        end = time.time()
-        
-        df.loc[i, f] = end - begin
+        for c in cmds:
+            begin = time.time()
+
+            p = subprocess.Popen(
+                cmd_str.format(c, i, f),
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
+
+            out, err = p.communicate()
+
+            end = time.time()
+
+            df.loc[i, f] = end - begin
 
 
 if __name__ == '__main__':
     main()
-
